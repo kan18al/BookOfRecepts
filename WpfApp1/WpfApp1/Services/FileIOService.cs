9f01cdbe8124345a91585ad9450ed4f4561310af
kan18al <31432042+kan18al@users.noreply.github.com>
2020-05-12 15:25:12 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/Services/FileIOService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace WpfApp1.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            this.PATH = path;
+        }
+
+        public ObservableCollection<Recept> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new ObservableCollection<Recept>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<ObservableCollection<Recept>>(fileText);
+            }
+        }
+
+        public void SaveData(ObservableCollection<Recept> recepts)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(recepts);
+                writer.Write(output);
+            }
+        }
+    }
+}
